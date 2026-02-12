b3d5f26b5853df446d5efebb67d6e5909aae08d6
Abdulrahman92c <99719388+Abdulrahman92c@users.noreply.github.com>
2026-02-12 12:01:18 +0100

    [REFAC] formattage de la liste d'objet de véhicule

--- src/Data/Vehicle.js
+++ src/Data/Vehicle.js
@@ -1,18 +1,33 @@
 const vehicles = [
 	{
-	marque: "Peugeot",modele: "208",annee: 2022,prix: 13500
+        marque: "Peugeot",
+        modele: "208",
+        annee:2022,
+        prix:13500
 	},
 	{
-    marque: "Renault",modele: "Clio", annee: 2023, prix: 12200
+        marque:"Renault",
+        modele:"Clio",
+        annee:2023,
+        prix:12200
 	},
     {
-    marque: "Citroen", modele: "C3", annee: 2009, prix: 9900  
+        marque:"Citroen",
+        modele:"C3",
+        annee:2009,
+        prix:9900  
     },
     {
-    marque: "Volkswagen", modele: "Golf", annee: 1998, prix: 14500
+        marque:"Volkswagen",
+        modele:"Golf",
+        annee:1998,
+        prix:14500
 	},
 	{
-	marque: "Toyota",modele: "Yaris",annee: 2022,prix: 15900
+        marque:"Toyota",
+        modele:"Yaris",
+        annee:2022,
+        prix:15900
 	}
 ];
 export default vehicles;
